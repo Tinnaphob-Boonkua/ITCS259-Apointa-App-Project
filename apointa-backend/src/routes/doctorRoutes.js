354845690bd8e3440345250accd3d480091c58a8
Tinnaphob-Boonkua <markzabk@gmail.com>
2025-11-20 19:31:06 +0700

    Delete apointa-backend/src directory

--- apointa-backend/src/routes/doctorRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getDoctors, getDoctorAvailability } from '../controllers/doctorController.js';
-
-const router = express.Router();
-
-router.get('/', getDoctors);
-router.get('/:id/availability', getDoctorAvailability);
-
-export default router;
